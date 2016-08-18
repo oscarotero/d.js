8a4be3d737113ce8f28b15c01305c1bf319881a8
oscarotero <oom@oscarotero.com>
2016-08-18 12:39:40 +0200

    new instance based api

--- d.js
+++ d.js
@@ -15,7 +15,7 @@
     // Helpers functions
 
     function selectAll(query, context) {
-        if (Array.isArray(query)) {
+        if (Array.isArray(query) || query instanceof d) {
             return query;
         }
 
@@ -35,7 +35,7 @@
             return (context || document).querySelector(query);
         }
 
-        if (Array.isArray(query) || query instanceof NodeList || query instanceof HTMLCollection) {
+        if (Array.isArray(query) || query instanceof NodeList || query instanceof HTMLCollection || query instanceof d) {
             return query[0];
         }
 
@@ -89,127 +89,172 @@
         }
     }
 
-    return {
+    function d (query, context) {
+        if (this instanceof d) {
+            var elements;
 
-        /*
-         * Select the first element
-         */
-        get: function (query, context) {
-            return selectOne(query, context);
-        },
+            if (typeof query === 'string' && query[0] === '<') {
+                elements = d.parse(query) || [];
 
-        /*
-         * Select an array of elements
-         */
-        getAll: function (query, context) {
-            return selectAll(query, context);
-        },
-
-        /*
-         * Check whether the element matches with a selector
-         */
-        is: function (element, query) {
-            if (typeof query === 'string') {
-                return (element.matches || element.matchesSelector || element.msMatchesSelector || element.mozMatchesSelector || element.webkitMatchesSelector || element.oMatchesSelector).call(element, query);
-            }
-
-            return element === query;
-        },
-
-        /*
-         * Attach an event to the elements.
-         */
-        on: function (event, query, callback, useCapture) {
-            return handleEvents(event, query, callback, useCapture, 'addEventListener');
-        },
-
-        /*
-         * Detach an event from the elements.
-         */
-        off: function (event, query, callback, useCapture) {
-            return handleEvents(event, query, callback, useCapture, 'removeEventListener');
-        },
-
-        /*
-         * Dispatch an event.
-         */
-        trigger: function (event, query) {
-            var elements = selectAll(query);
-
-            if (typeof event === 'string') {
-                if (window.Event) {
-                    event = new Event(event);
-                } else {
-                    event = document.createEvent('Event');
-                    event.initEvent(event, true, true);
+                if (!Array.isArray(elements)) {
+                    elements = [elements];
                 }
-            }
-
-            elements.forEach(function (element) {
-                element.dispatchEvent(event);
-            });
-
-            return elements;
-        },
-
-        /*
-         * Remove elements
-         */
-        remove: function (query) {
-            selectAll(query).forEach(function (element) {
-                element.parentNode.removeChild(element);
-            });
-        },
-
-        /*
-         * Get/set the styles of elements
-         */
-        css: function (query, prop, value) {
-            if (value === undefined && (typeof prop !== 'object')) {
-                var style = getComputedStyle(selectOne(query));
-
-                if (prop === undefined) {
-                    return style;
-                }
-
-                return style[styleProp(prop)];
-            }
-
-            var rules = {};
-
-            if (typeof prop === 'object') {
-                rules = prop;
             } else {
-                rules[prop] = value;
+                elements = selectAll(query, context);
             }
 
-            var elements = selectAll(query);
-            
-            elements.forEach(function (element, index, elements) {
-                for (var prop in rules) {
-                    element.style[styleProp(prop)] = (typeof rules[prop] === 'function') ? rules[prop].call(this, element, index, elements) : rules[prop];
-                }
-            });
-
-            return elements;
-        },
-
-        /*
-         * Parses a html code
-         */
-        parse: function (html) {
-            var tmp = document.implementation.createHTMLDocument();
-            tmp.body.innerHTML = html;
-
-            if (tmp.body.children.length === 0) {
-                return null;
-            }
-
-            if (tmp.body.children.length === 1) {
-                return tmp.body.children[0];
-            }
-
-            return selectAll(tmp.body.children);
+            Array.prototype.splice.apply(this, [0, 0].concat(elements));
+        } else {
+            return new d(query, context);
         }
     };
+
+    d.prototype = Object.create(Array.prototype, {
+        on: {
+            value: function (event, callback, useCapture) {
+                return d.on(event, this, callback, useCapture);
+            }
+        },
+        off: {
+            value: function (event, callback, useCapture) {
+                return d.off(event, this, callback, useCapture);
+            }
+        },
+        trigger: {
+            value: function (event) {
+                return d.trigger(event, this);
+            }
+        },
+        css: {
+            value: function (prop, value) {
+                var args = Array.prototype.slice.call(arguments);
+                args.unshift(this);
+
+                return d.css.apply(null, args);
+            }
+        }
+    });
+
+    /*
+     * Select the first element
+     */
+    d.get = function (query, context) {
+        return selectOne(query, context);
+    };
+
+    /*
+     * Select an array of elements
+     */
+    d.getAll = function (query, context) {
+        return selectAll(query, context);
+    };
+
+    /*
+     * Check whether the element matches with a selector
+     */
+    d.is = function (element, query) {
+        if (typeof query === 'string') {
+            return (element.matches || element.matchesSelector || element.msMatchesSelector || element.mozMatchesSelector || element.webkitMatchesSelector || element.oMatchesSelector).call(element, query);
+        }
+
+        return element === query;
+    };
+
+    /*
+     * Attach an event to the elements.
+     */
+    d.on = function (event, query, callback, useCapture) {
+        return handleEvents(event, query, callback, useCapture, 'addEventListener');
+    };
+
+    /*
+     * detach an event from the elements.
+     */
+    d.off = function (event, query, callback, useCapture) {
+        return handleEvents(event, query, callback, useCapture, 'removeEventListener');
+    };
+
+    /*
+     * dispatch an event.
+     */
+    d.trigger = function (event, query) {
+        var elements = selectAll(query);
+
+        if (typeof event === 'string') {
+            if (window.Event) {
+                event = new Event(event);
+            } else {
+                event = document.createEvent('Event');
+                event.initEvent(event, true, true);
+            }
+        }
+
+        elements.forEach(function (element) {
+            element.dispatchEvent(event);
+        });
+
+        return elements;
+    };
+
+    /*
+     * Remove elements
+     */
+    d.remove = function (query) {
+        selectAll(query).forEach(function (element) {
+            element.parentNode.removeChild(element);
+        });
+    };
+
+    /*
+     * Get/set the styles of elements
+     */
+    d.css = function (query, prop, value) {
+        if (arguments.length < 3 && (typeof prop !== 'object')) {
+            var style = getComputedStyle(selectOne(query));
+
+            if (arguments.length === 1) {
+                return style;
+            }
+
+            return style[styleProp(prop)];
+        }
+
+        var rules = {};
+
+        if (typeof prop === 'object') {
+            rules = prop;
+        } else {
+            rules[prop] = value;
+        }
+
+        var elements = selectAll(query);
+
+        elements.forEach(function (element, index, elements) {
+            for (var prop in rules) {
+                element.style[styleProp(prop)] = (typeof rules[prop] === 'function') ? rules[prop].call(this, element, index, elements) : rules[prop];
+            }
+        });
+
+        return elements;
+    };
+
+    /*
+     * Parses a html code
+     */
+    d.parse = function (html) {
+        var tmp = document.implementation.createHTMLDocument();
+        tmp.body.innerHTML = html;
+
+        if (tmp.body.children.length === 0) {
+            return null;
+        }
+
+        if (tmp.body.children.length === 1) {
+            return tmp.body.children[0];
+        }
+
+        return selectAll(tmp.body.children);
+    };
+
+    return d;
 }));
